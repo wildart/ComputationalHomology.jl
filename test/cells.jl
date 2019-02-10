024db59e6aa400e4ad89671ba9d60ab1ee0c2a20
Art Wild <wildart@gmail.com>
2019-02-10 12:36:26 -0500

    removed dead code

--- test/cells.jl
+++ test/cells.jl
@@ -17,20 +17,14 @@
     push!(ss, s)
     @test length(ss) == 1
 
-    function Base.isless(a::Vector{Int},b::Vector{Int})
-        length(a) < length(b) && return true
-        length(a) > length(b) && return false
-        return any([aa < bb for (aa,bb) in zip(a,b)])
-    end
-    @test [1] < [1,3]
-    @test [1,2,1] > [1,3]
-    @test [1,2] < [1,3]
     s = Simplex([1,2], [1,3])
     @test dim(s) == 1
 
     X = hcat(zeros(3), Matrix(I,3,3))
     S = Simplex(1,2,3,4)
     @test volume(S,X) == 1/6
+
+    @test faces(s)[1] ∪ faces(s)[2] == s
 end
 
 @testset "Cube" begin
